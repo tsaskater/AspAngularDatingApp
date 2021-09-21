f233f2bde064af5647dc2812aa7e4f8700e8757f
Ács Péter <parasztragu@gmail.com>
2021-09-21 16:33:27 +0200

    ui updated to register and login

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  baseurl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User | null>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+  constructor(private http: HttpClient) {}
+  login(model: any) {
+    return this.http.post<User>(this.baseurl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response as User;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+  register(model: any) {
+    return this.http.post<User>(this.baseurl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
