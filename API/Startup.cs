927d87cf16dd605bd539ab194ee47bcede7eeaf2
Ács Péter <parasztragu@gmail.com>
2021-10-12 14:25:50 +0200

    heroku initial

--- API/Startup.cs
+++ API/Startup.cs
@@ -67,12 +67,14 @@ namespace API
       });
       app.UseAuthentication();
       app.UseAuthorization();
-
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
         endpoints.MapHub<PresenceHub>("hubs/presence");
         endpoints.MapHub<MessageHub>("hubs/message");
+        endpoints.MapFallbackToController("Index", "Fallback");
       });
     }
   }
